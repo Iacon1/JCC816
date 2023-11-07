a23db92a3f4bae3ba10fc170c3f493e09ed3ec83
Iacon1 <vos6mya@gmail.com>
2023-11-06 22:02:12 -0500

    Fixed DeclaratorNode's pointer detection

--- src/Compiler/ComponentNodes/Declarations/DeclaratorNode.java
+++ src/Compiler/ComponentNodes/Declarations/DeclaratorNode.java
@@ -8,6 +8,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import Compiler.ComponentNodes.ComponentNode;
 import Compiler.ComponentNodes.InterpretingNode;
 import Compiler.ComponentNodes.Declarations.DirectDeclaratorNode.DirDeclaratorInfo;
@@ -22,7 +25,7 @@ public class DeclaratorNode extends InterpretingNode<DeclaratorNode, DeclaratorC
 	private List<Set<String>> pointerQualifiers;
 	private DirectDeclaratorNode directDeclarator;
 	
-	public DeclaratorNode(ComponentNode<?> parent) {super(parent);}
+	public DeclaratorNode(ComponentNode<?> parent) {super(parent); pointerQualifiers = new LinkedList<Set<String>>();}
 	public DeclaratorNode() {super(); pointerQualifiers = new LinkedList<Set<String>>();}
 	
 	@Override
@@ -30,15 +33,18 @@ public class DeclaratorNode extends InterpretingNode<DeclaratorNode, DeclaratorC
 	{
 		// Get pointer(s)
 		if (node.pointer() != null)
-		{
-			for (Type_qualifier_listContext qualList : node.pointer().type_qualifier_list())
-			{
-				List<String> qualifiers = new LinkedList<String>();
-				for (Type_qualifierContext qualifier : qualList.type_qualifier())
-					qualifiers.add(qualifier.getText());
-				pointerQualifiers.add(new HashSet<String>(qualifiers));
-			}
-		}
+			for (int i = 0; i < node.pointer().getChildCount(); ++i)
+				if (node.pointer().getChild(i).getText().equals("*")) // Start of a pointer level
+					if (i < node.pointer().getChildCount() - 1) // Another after this
+						if (!node.pointer().getChild(i + 1).getText().equals("*")) // Next one's not start of a pointer level
+						{
+							List<String> qualifiers = new LinkedList<String>();
+							for (Type_qualifierContext qualifier : ((Type_qualifier_listContext) node.pointer().getChild(i + 1)).type_qualifier())
+								qualifiers.add(qualifier.getText());
+							pointerQualifiers.add(new HashSet<String>(qualifiers));
+						}
+						else pointerQualifiers.add(new HashSet<String>());
+					else pointerQualifiers.add(new HashSet<String>());	
 		
 		directDeclarator = new DirectDeclaratorNode(this).interpret(node.direct_declarator());
 		
