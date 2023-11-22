26e1b641beb84bf6578a49c3fcf9dec28998de6d
Iacon1 <vos6mya@gmail.com>
2023-11-21 21:51:51 -0500

    Implemented ConditionalExpressionNode

--- src/Compiler/ComponentNodes/Expressions/ConditionalExpressionNode.java
+++ src/Compiler/ComponentNodes/Expressions/ConditionalExpressionNode.java
@@ -4,20 +4,23 @@ package Compiler.ComponentNodes.Expressions;
 
 import Compiler.ComponentNodes.ComponentNode;
 import Compiler.ComponentNodes.FunctionDefinitionNode;
+import Compiler.Utils.AssemblyUtils;
 import Compiler.Utils.AssemblyUtils.DetailsTicket;
 import Compiler.Utils.ScratchManager;
+import Compiler.Utils.ScratchManager.ScratchSource;
 import Compiler.Utils.OperandSources.OperandSource;
 import Grammar.C99.C99Parser.Conditional_expressionContext;
+import Grammar.C99.C99Parser.ExpressionContext;
 import Grammar.C99.C99Parser.Lor_expressionContext;
 
 // TODO conditional expression
 public class ConditionalExpressionNode extends BinaryExpressionNode
-<Lor_expressionContext, Conditional_expressionContext, Lor_expressionContext, Conditional_expressionContext>
+<ExpressionContext, Conditional_expressionContext, Lor_expressionContext, Conditional_expressionContext>
 {
 	private BaseExpressionNode<?> z;
 	@Override
-	protected BaseExpressionNode<Lor_expressionContext> getC1Node(Conditional_expressionContext node) throws Exception
-	{return new LORExpressionNode(this).interpret(node.lor_expression());}
+	protected BaseExpressionNode<ExpressionContext> getC1Node(Conditional_expressionContext node) throws Exception
+	{return new ExpressionNode(this).interpret(node.expression());}
 	@Override
 	protected BaseExpressionNode<Conditional_expressionContext> getC2Node(Conditional_expressionContext node) throws Exception
 		{return new ConditionalExpressionNode(this).interpret(node.conditional_expression());}
@@ -32,7 +35,7 @@ public class ConditionalExpressionNode extends BinaryExpressionNode
 			return (BaseExpressionNode) getPCNode(node);
 		else // Two
 		{
-			z = (BaseExpressionNode) new ExpressionNode(this).interpret(node.expression());
+			z = (BaseExpressionNode) new LORExpressionNode(this).interpret(node.lor_expression());
 			x = getC1Node(node);
 			y = getC2Node(node);
 			
@@ -56,15 +59,54 @@ public class ConditionalExpressionNode extends BinaryExpressionNode
 	@Override
 	public Object getPropValue()
 	{
-		if ((Boolean) z.getPropValue()) return x.getPropValue();
+		if (z.getPropBool()) return x.getPropValue();
 		else return y.getPropValue();
 	}
 	@Override
 	protected String getAssembly(String whitespace, OperandSource destSource, OperandSource sourceX,
 			OperandSource sourceY, ScratchManager scratchManager, DetailsTicket ticket) throws Exception
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String assembly = "";
+		if (!z.hasPropValue())
+		{
+			if (z.hasAssembly())
+			{
+				OperandSource sourceZ;
+				if (z.hasLValue())
+				{
+					sourceZ = z.getLValue().getSource();
+					assembly += z.getAssembly(whitespace.length(), null, scratchManager, ticket);
+					assembly += EqualityExpressionNode.getIsZero(whitespace, null, scratchManager, sourceZ, ticket);
+				}
+				else
+				{
+					sourceZ = scratchManager.reserveScratchBlock(z.getSize());
+					assembly += z.getAssembly(whitespace.length(), sourceZ, scratchManager, ticket);
+					if (z.getSize() > 1)
+						assembly += EqualityExpressionNode.getIsZero(whitespace, null, scratchManager, sourceZ, ticket);
+					scratchManager.releaseScratchBlock((ScratchSource) sourceZ);
+				}		
+			}
+			else if (z.hasLValue())
+			{
+				OperandSource sourceZ = z.getLValue().getSource();
+				assembly += EqualityExpressionNode.getIsZero(whitespace, null, scratchManager, sourceZ, ticket);
+			}
+			assembly += whitespace + "BEQ\t:+\n";
+			assembly += AssemblyUtils.byteCopier(whitespace, sourceX.getSize(), destSource, sourceX);
+			assembly += whitespace + "BRA\t:+\n";
+			assembly += AssemblyUtils.byteCopier(whitespace, sourceY.getSize(), destSource, sourceY);
+			assembly += whitespace + ":\n";
+		}
+		else
+		{
+			if (z.getPropBool())
+				assembly += AssemblyUtils.byteCopier(whitespace, sourceX.getSize(), destSource, sourceX);
+			else
+				assembly += AssemblyUtils.byteCopier(whitespace, sourceY.getSize(), destSource, sourceY);
+		}
+		
+		return assembly;
 	}
 	
 }
