00086fa8b4fb7c1997f3189a076ba8f269c4b1e9
Iacon1 <vos6mya@gmail.com>
2024-05-26 15:11:23 -0400

    .

--- src/Executables/Test.java
+++ src/Executables/Test.java
@@ -11,9 +11,5 @@ public class Test
 	{
 		FileIO.outputFolder = "Test/";
 //		JCC816.main(new String[] {"-r=Example/", "-l=example=exampleHeader.xml", "-O=1", "-D=2", "-V=2", "example.c"});
-//		JCC816.main(new String[] {"-h=Example/exampleHeader.xml", "-l=Example/example", "-o=0", "-d=2", "-v=2", "-r=Example/", "SNES/Graphics/oam.c"});
-//		JCC816.main(new String[] {"-h=Example/exampleHeader.xml", "-p=Example/exampleP.c", "-o=0", "-d=2", "-v=2", "-r=Example/", "Example/example.c"});
-//		JCC816.main(new String[] {"-r=Test/", "-l=test=testHeader.xml", "-O=2", "-D=2", "-V=2", "test.c"});
-//		JCC816.main(new String[] {"-r=../Witchcraft/src/", "-l=test=header.xml", "-O=2", "-D=2", "-V=2", "main.c"});
 	}
 }
