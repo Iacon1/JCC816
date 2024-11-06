80c5024462d14a75a633f4b7dda0b9af66c56101
Iacon1 <vos6mya@gmail.com>
2024-11-05 21:51:47 -0500

    Added proper return value to printf.

--- src/stdlib/stdio.c
+++ src/stdlib/stdio.c
@@ -4,12 +4,14 @@
 int printf(const char* string)
 {
 	extern char destination;
+	int len = 0;
 	asm("printf@destination = $006000");
 	while (*string != '\0')
 	{
 		destination = *string;
 		string += 1;
+		len += 1;
 	}
 	destination = '\0';
-	return 0;
+	return len;
 }
