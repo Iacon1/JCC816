568fb7840a0f536e0bdd54bb40be92f27d5f2bbe
Iacon1 <vos6mya@gmail.com>
2025-02-14 20:13:52 -0500

    Fix LANDExpressionNode being backwards for some reason

--- src/C99Compiler/CompilerNodes/Expressions/LANDExpressionNode.java
+++ src/C99Compiler/CompilerNodes/Expressions/LANDExpressionNode.java
@@ -13,7 +13,7 @@ import Grammar.C99.C99Parser.Land_expressionContext;
 import Grammar.C99.C99Parser.Or_expressionContext;
 
 public class LANDExpressionNode extends LogicalBinaryExpressionNode
-<Or_expressionContext, Land_expressionContext, Or_expressionContext, Land_expressionContext>
+<Land_expressionContext, Or_expressionContext, Or_expressionContext, Land_expressionContext>
 {
 	private static class LANDOperator extends BytewiseOperator
 	{
@@ -42,12 +42,12 @@ public class LANDExpressionNode extends LogicalBinaryExpressionNode
 	public LANDExpressionNode(ComponentNode<?> parent) {super(parent);}
 
 	@Override
-	protected BaseExpressionNode<Or_expressionContext> getC1Node(Land_expressionContext node) throws Exception
-	{return new OrExpressionNode(this).interpret(node.or_expression());}
+	protected BaseExpressionNode<Land_expressionContext> getC1Node(Land_expressionContext node) throws Exception
+	{return new LANDExpressionNode(this).interpret(node.land_expression());}
 
 	@Override
-	protected BaseExpressionNode<Land_expressionContext> getC2Node(Land_expressionContext node) throws Exception
-	{return new LANDExpressionNode(this).interpret(node.land_expression());}
+	protected BaseExpressionNode<Or_expressionContext> getC2Node(Land_expressionContext node) throws Exception
+	{return new OrExpressionNode(this).interpret(node.or_expression());}
 
 	@Override
 	protected BaseExpressionNode<Or_expressionContext> getPCNode(Land_expressionContext node) throws Exception
