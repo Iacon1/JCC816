51c1ac0d2526a85bef47c84e91faf575867de3e8
Iacon1 <vos6mya@gmail.com>
2025-06-16 20:58:20 -0400

    Fix bad variable name in stdio.c.

--- src/stdlib/stdio.c
+++ src/stdlib/stdio.c
@@ -5,10 +5,10 @@ int puts(const char* s)
 	extern char destination;
 	int len = 0;
 	asm("printf@destination = $006000");
-	while (*string != '\0')
+	while (*s != '\0')
 	{
 		destination = *string;
-		string += 1;
+		s += 1;
 		len += 1;
 	}
 	destination = '\0';
