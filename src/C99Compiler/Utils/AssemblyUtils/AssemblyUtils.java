a701d1870b4821695167981b55a1191a57408b99
Iacon1 <vos6mya@gmail.com>
2025-08-19 17:28:45 -0400

    Add ForceFlags AssemblyUtil

--- src/C99Compiler/Utils/AssemblyUtils/AssemblyUtils.java
+++ src/C99Compiler/Utils/AssemblyUtils/AssemblyUtils.java
@@ -11,6 +11,10 @@ import C99Compiler.Utils.CompUtils;
 import C99Compiler.Utils.ProgramState;
 import C99Compiler.Utils.OperandSources.OperandSource;
 import C99Compiler.Utils.OperandSources.StationaryAddressSource;
+import C99Compiler.Utils.ProgramState.PreserveFlag;
+import C99Compiler.Utils.ProgramState.ProcessorFlag;
+import Shared.Assemblable.AssemblyStatePair;
+import Shared.Assemblable.MutableAssemblyStatePair;
 
 public final class AssemblyUtils
 {
@@ -51,6 +55,41 @@ public final class AssemblyUtils
 		return assembly;
 	}
 
+	public static MutableAssemblyStatePair forceFlags(MutableAssemblyStatePair pair, byte flags, boolean set)
+	{
+		byte procFlags = 0;
+		if ((flags & PreserveFlag.M) != 0)
+			procFlags |= ProcessorFlag.M;
+		if ((flags & PreserveFlag.I) != 0)
+			procFlags |= ProcessorFlag.I;
+		
+		String procString = String.format("%02x", procFlags);
+		if (set)
+		{
+			if (!pair.state.testKnownFlag(flags) || !pair.state.testProcessorFlag(procFlags))
+			{
+				pair.assembly += pair.state.getWhitespace() + "REP\t#$" + procString + "\n";
+				pair.state = pair.state.setProcessorFlags(ProcessorFlag.I);				
+			}
+		}
+		else
+		{
+			if (!pair.state.testKnownFlag(flags) || pair.state.testProcessorFlag(procFlags))
+			{
+				pair.assembly += pair.state.getWhitespace() + "SEP\t#$" + procString + "\n";
+				pair.state = pair.state.clearProcessorFlags(ProcessorFlag.I);				
+			}
+		}
+		
+		return pair;
+	}
+	
+	public static AssemblyStatePair forceFlags(AssemblyStatePair pair, byte flags, boolean set)
+	{
+		MutableAssemblyStatePair tmpPair = new MutableAssemblyStatePair(pair.assembly, pair.state);
+		return forceFlags(tmpPair, flags, set).getImmutable();
+	}
+	
 	public static OperandSource getShrinkWrapped(ProgramState state, LValueNode<?> x)
 	{
 		if (state.getSmallestValue(x) != null && state.getLargestValue(x) != null)
