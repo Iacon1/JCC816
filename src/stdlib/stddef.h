d1019d131d4432e0c22ae9a259079dce0458335d
Iacon1 <vos6mya@gmail.com>
2025-09-14 23:27:24 -0400

    Include guard for stddef

--- src/stdlib/stddef.h
+++ src/stdlib/stddef.h
@@ -1,8 +1,10 @@
 // Common definitions
-
+#ifndef STDDEF_H
+#define STDDEF_H
 typedef signed int ptrdiff_t;
 typedef unsigned int size_t;
 typedef char wchar_t;
 
 #define NULL 0
 #define offsetof(st, m) __offset_of(st, m)
+#endif
